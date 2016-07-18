c4acf4dc3aad38469433dcea0bd4ac6d69e99c41
Peter Ciporin <pciporin@gmail.com>
2016-07-18 12:30:53 -0400

    Updated package.json, general cleanup

--- generator.js
+++ generator.js
@@ -1,13 +1,9 @@
 var request = require('request');
-var cheerio = require('cheerio');
 
 var quoteId = Math.floor((Math.random()*4194)+1);
 let quote = null;
 let author = null;
 var exports = module.exports = {};
-
-//console.log(quoteId);
-//console.log(`http://www.quotedb.com/quotes/${quoteId}`);
 
 var options = {
   url: `http://www.quotedb.com/quotes/${quoteId}`
@@ -15,43 +11,20 @@ var options = {
 
 function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
-    //console.log(body) // Show the HTML for the Google homepage.
-    //var $ = cheerio.load(body.toString());
+
     var strung = body.toString();
+
     var start = strung.search("title")+6;
-    //console.log("start is ", start);
     var end = strung.search("</title");
-    //console.log("end is ", end);
-    //var extracted = strung.substring(start,end);
+
     var extracted = strung.substring(start,end);
     var authIndex = extracted.search(". by ");
 
-    /*
-    exports.quote = function() {
-      return extracted.substring(0,authIndex+1);
-    };
-
-    exports.author = function() {
-      return extracted.substring(authIndex+5);
-    };
-    */
-
-    //console.log("extracted is",extracted);
-
-
-    //var authIndex = extracted.search(". by ");
-    //console.log("authIndex is",authIndex);
     quote = extracted.substring(0,authIndex+1);
-    //console.log("quote is ",quote);
     author = extracted.substring(authIndex+5);
-    //console.log("\n\n\nauthor is ",author);
-    //console.log($('title').find('title'));
-    setExports();
-    exports.quote = quote;
 
+    exports.quote = quote;
     exports.author = author;
-    //console.log("\nran!!");
-    //console.log("quote is",quote);
   }
   else {
     console.error(error);
@@ -59,15 +32,3 @@ function callback(error, response, body) {
 }
 
 exports.run = request(options,callback);
-
-function setExports() {
-
-}
-
-/*
-function extractData() {
-  request(`http://www.quotedb.com/quotes/${quoteId}`, function (error, response, body) {
-
-  });
-};
-*/
--- index.js
+++ index.js
@@ -9,12 +9,9 @@ generator.run;
 
 function isReady(nullCheck) {
   if (nullCheck === null || typeof nullCheck == 'undefined') {
-    //console.log("author is",author);
-    //console.log(generator.author);
     return false;
   }
   else {
-    //console.log("author is not null or undefined")
     return true;
   }
 }
@@ -24,16 +21,14 @@ function logging(callback) {
     console.log("\nLoading...please wait a moment while we generate your quote.");
     first = false;
   }
-  //console.log(author());
   author = generator.author
   quote = generator.quote;
-  //console.log(author);
   setTimeout(function(){ callback(author); }, 3000);
 };
 
 function ready(nullCheck) {
   console.log("\n\nYour quote has been generated!");
-  console.log(`\nToday's quote belongs to ${author}!`);
+  console.log(`\nThis quote belongs to ${author}:`);
   console.log(`\n${quote}\n--${author}\n`);
 }
 
